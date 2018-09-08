888d588f18a927750e6c38511153edc5569af314
Benjamin Hurley <BenHurley@wc-dhcp191d151.student-secure.wireless.fsu.edu>
2018-09-08 13:40:24 -0400

    [Feature] Prints original broken message to console ✨

--- cn2.pl
+++ cn2.pl
@@ -4,12 +4,10 @@
 # Computer Networks Program 2
 
 use warnings;
-
+   
+# (Function) calcSyndrome: Calculates the syndrome and finds a match in the
+# parity matrix to determine which bit needs to be flipped
 sub calcSyndrome {
-
-   # Calculates the syndrome and finds a match in the
-   # parity matrix to determine which bit needs to 
-   # be flipped
 
    @codeword = @{$_[0]};
 
@@ -65,9 +63,8 @@ sub calcSyndrome {
    return @fourTuple;
 }
 
+# (Function) covert: Converts a 7 bit codeword to the 4 bit piece needed
 sub convert {
-
-   # Converts a 7 bit codeword to the 4 bit piece needed
 
    @codeword = @{$_[0]};
    my @temp;
@@ -79,26 +76,42 @@ sub convert {
    return @temp;
 }
 
-# Main Variables:
+#-----------------------------------
+# Main program
+
+# global variables:
+
 my @array;
 my @codeword;
-my $spaces;
 my @fixed;
 my @build;
-my $add = 0;
 my @four;
 
-# open file and read in contents:
+my $spaces;
+my $add = 0;
+my $addOld = 0;
 
-open(my $fh, "<", "corrupted_email_2") or die "Test data file not found in directory\n";
+# save arguments, check if valid:
+my ($filename) = @ARGV;
+
+if(not defined $filename) {
+    die "\nInvalid run. Need to pass in corrupted email as argument.\n\n";
+}
+
+# open corrupted email
+open(my $fh, "<", $ARGV[0]) or die "\nAn error has occured opening the file.\n\n";
+
+print"\n----------------------------------\n";
+print "Request to fix: $filename\n";
+print"----------------------------------\n";
+
+# push corrupted email into array
 
 while(<$fh>) { 
     chomp; 
     push @array, $_;
 } 
 close $fh;
-
-# testdata is saved
 
 # using regex, strip whitespace and pass codewords into 
 # function to calculate the syndrome:
@@ -108,14 +121,35 @@ foreach $_ (@array) {
    $spaces =~ s/\s\s+//g;
    $spaces =~ s/^\s+//;
 
+   # remove spaces
    @codeword = split('', $spaces, length($spaces));
-   @four = calcSyndrome(\@codeword);
 
-   # this is where corrected bits-trings (length 4) return
+   # store old codeword to print original message
+   my @oldTuple = convert(\@codeword);
+   push @old, @oldTuple;
+
+   # this is where corrected bit-strings (length 4) return
+   @four = calcSyndrome(\@codeword);
    push @build, @four;
 }
 
+# print original corrupted message:
+print "\n----------------------------------------\n";
+print "Previous corrupted version of the email:\n";
+print "----------------------------------------\n\n";
+
+for (my $i = 0; $i < scalar @old; $i = $i + 8)
+{
+   my $addOld = 128*$old[$i] + 64*$old[$i+1] + 32*$old[$i+2] + 16*$old[$i+3] + 8*$old[$i+4] + 4*$old[$i+5] + 2*$old[$i+6] + 1*$old[$i+7];
+
+   my $letterOld = chr($addOld);
+   print "$letterOld";
+}
+
 # convert into ASCII to print message:
+print"\n\n-------------------------------------------\n";
+print "Corrected Version using (7,4) Hamming Code:\n";
+print"-------------------------------------------\n\n";
 
 for (my $i = 0; $i < scalar @build; $i = $i + 8)
 {
